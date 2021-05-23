b5dee074b5474e941642542dd52bc826ba7417eb
arthur-pe <pellegrino.arthur@gmail.com>
2021-05-23 16:57:22 +0200

    FORCE learning v0

--- classes.py
+++ classes.py
@@ -2,35 +2,36 @@ import numpy as np
 
 class RNN:
 
-    def __init__(self, number_neurons, output_dim, alpha, time_bin_length):
+    def __init__(self, N_G, output_dim, alpha, tau, g_G_G, g_G_F, g_Gz):
 
         #Right multiplications
-        self.recurrent_weights = np.random.randn((number_neurons,number_neurons))
-        self.decoder = np.random.randn((number_neurons,output_dim))
-        self.feedback = np.random.randn((output_dim,number_neurons))
+        self.J_G_G = np.random.randn((N_G,N_G)) #recurrent weights
+        self.w = np.random.randn((N_G,output_dim)) #decoder
+        self.J_Gz = np.random.randn((output_dim,N_G)) #feedback weights
 
-        self.neurons_state = np.random.randn(number_neurons)
-        self.output = np.matmul(self.neurons_state,self.decoder)
-        self.P = np.ones((number_neurons,number_neurons))/alpha
+        self.g_G_G = g_G_G
+        self.g_Gz = g_Gz
 
-        self.number_neurons = number_neurons
-        self.output_dim = output_dim
-        self.time_bin_length = time_bin_length
+        self.r = np.random.randn(N_G) #neuron state
+        self.z = np.matmul(self.r,self.w) #output
+        self.P = np.ones((N_G,N_G))/alpha
+
+        self.N_G = N_G #number neurons in generator
+        self.output_dim = output_dim #1
+        self.tau = tau #time bin
         self.alpha = alpha
 
     def step(self):
 
-        self.neurons_state = self.neurons_state + \
-                             self.time_bin_length*(np.matmul(self.neurons_state,self.recurrent_weights)
-                                      +np.matmul(self.output, self.feedback))
+        self.r = np.tanh(self.r + self.tau*(self.g_G_G*self.r @ self.J_G_G + self.g_Gz*self.z @ self.J_Gz))
 
-        self.output = np.matmul(self.neurons_state, self.decoder)
+        self.z = np.matmul(self.r, self.w)
 
-        return self.neurons_state, self.output
+        return self.r, self.z
 
     def update(self, error, response):
 
         self.P = self.P - (self.P @ response @ response.transpose() @ self.P)/(1+response.transpose() @ self.P @ response)
 
-        self.decoder = self.decoder - error @ self.P @ response
+        self.w = self.w - error @ self.P @ response
 
