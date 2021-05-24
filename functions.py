39a1de519496209ea8104b0b9b6137160d841e52
sepehr <sepehr.rasouli94@gmail.com>
2021-05-24 12:37:20 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 
 # wave target functions
 
+
 def complex_periodic_func(t, n_sinusoids=16, amp=2., freq=1 / 300):
     """returns a complex periodic function with n sinosoids."""
     np.random.seed(4)
@@ -12,17 +13,23 @@ def complex_periodic_func(t, n_sinusoids=16, amp=2., freq=1 / 300):
                for i in range(1, n_sinusoids + 1))
 
 
-t = np.linspace(-20,600,500)
-plt.plot(t, complex_periodic_func(t))
-plt.show()
+def plot_complex_periodic_func():
+    plt.figure(dpi=300)
+    t = np.linspace(-20,600,500)
+    plt.plot(t, complex_periodic_func(t))
+    plt.show()
+
 
 def triangle_func(t, freq=1/300, amp=3.):
     """triangle wave function."""
     return amp * signal.sawtooth(2 * np.pi * freq * t, 0.5)
 
-t = np.linspace(0,1200,500)
-# plt.pltlot(t, triangle_func(t))
-plt.show()
+
+def plot_triangle_func():
+    plt.figure(dpi=300)
+    t = np.linspace(0,1200,500)
+    plt.plot(t, triangle_func(t))
+    plt.show()
 
 
 def periodic_func(t, n_sinusoids=4, amp=3., freq=1/300, noise=False):
@@ -33,13 +40,12 @@ def periodic_func(t, n_sinusoids=4, amp=3., freq=1/300, noise=False):
     else:
         return my_sum
 
-plt.figure(dpi=300)
-t = np.linspace(0,1800,500)
-plt.plot(t, periodic_func(t))
-plt.show()
 
-plt.plot(t, periodic_func(t, noise=True))
-plt.show()
+def plot_periodic_func(noise=False):
+    plt.figure(dpi=300)
+    t = np.linspace(0,1800,500)
+    plt.plot(t, periodic_func(t, noise=noise))
+    plt.show()
 
 
 def square_func(t, noise=False):
@@ -50,11 +56,11 @@ def square_func(t, noise=False):
     else:
         return my_sum
 
-t = np.linspace(0,400,500)
-plt.plot(t, square_func(t))
-plt.ylim(-2, 2)
-plt.show()
 
-plt.plot(t, square_func(t,noise=True))
-plt.ylim(-2, 2)
-plt.show()
+def plot_square_func(noise=False):
+    plt.figure(dpi=300)
+    t = np.linspace(0,400,500)
+    plt.plot(t, square_func(t,noise=noise))
+    plt.ylim(-2, 2)
+    plt.show()
+
