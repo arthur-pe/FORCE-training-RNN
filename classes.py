8f64761c8067dff5c2bc5958316212480af216da
arthur-pe <pellegrino.arthur@gmail.com>
2021-05-28 10:35:16 +0200

    everything works

--- classes.py
+++ classes.py
@@ -11,18 +11,13 @@ class RNN:
         #self.w = np.zeros((N_G,output_dim))/np.sqrt(N_G)
         self.J_Gz = (2*np.random.rand(N_G,output_dim) - 1) #feedback weights
 
-        """self.J_G_G_mask = np.zeros(N_G**2)
-        self.J_G_G_mask[:int(N_G**2*p_G_G)] = 1
-        np.random.shuffle(self.J_G_G_mask)
-        self.J_G_G_mask = self.J_G_G_mask.reshape(N_G,N_G)"""
-
         self.g_G_G = g_G_G
         self.g_Gz = g_Gz
 
-        self.x = np.random.randn(N_G)*0.5
+        self.x = np.random.randn(N_G)
         self.r = np.tanh(self.x) #neuron state
 
-        self.z = np.random.randn(1)*0.5 #output np.matmul(self.r,self.w)
+        self.z = np.random.randn(1) #output np.matmul(self.r,self.w)
         self.P = np.eye(N_G)/alpha
 
         self.N_G = N_G #number neurons in generator
@@ -30,18 +25,8 @@ class RNN:
         self.tau = tau #time bin
         self.alpha = alpha
 
-    """def step(self, dt):
-
-        self.x += (dt/self.tau)*(-self.x+self.r @ self.J_G_G + self.z @ self.J_Gz)
-
-        self.r = np.tanh(self.x)
-
-        self.z = np.matmul(self.r, self.w)
-
-        return self.z, self.r"""
-
     def step(self, dt):
-        self.x = (1 - dt / self.tau) * self.x + self.g_G_G*self.J_G_G.dot(self.r) * dt / self.tau +  self.g_Gz*self.J_Gz.dot(self.z) * dt / self.tau
+        self.x += (- dt/self.tau) *(self.x + self.g_G_G*self.J_G_G.dot(self.r) + self.g_Gz*self.J_Gz.dot(self.z))
         self.r = np.tanh(self.x)
         self.z = self.w.T.dot(self.r).flatten()
 
@@ -49,17 +34,11 @@ class RNN:
 
     def update(self, error):
 
-        #self.P -= (((self.P @ self.r) @ self.r.transpose()) * self.P)/(1+self.r.transpose() @ self.P @ self.r)
-
         Pr = self.P.dot(self.r)
         self.P -= np.outer(Pr, self.r).dot(self.P) / (1 + self.r.dot(Pr))
 
         dw = np.outer(self.P.dot(self.r), error)
         self.w -= dw
-
-        """w_dot = np.expand_dims((error * self.P @ self.r),1)
-
-        self.w -= w_dot"""
 
         return np.sqrt(np.sum(dw**2))
 
--- functions.py
+++ functions.py
@@ -4,13 +4,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 
-# wave target functions
-
-
 def complex_periodic_func(t, n_sinusoids=16, amp=2., freq=1 / 300):
     """returns a complex periodic function with n sinosoids."""
     np.random.seed(4)
-    return sum(amp/i * np.random.randint(1,5) * np.sin(i * np.random.randint(1,2) * np.pi * freq * t)
+    return sum(0.4*amp/i * np.random.randint(1,5) * np.sin(i * np.random.randint(1,2) * np.pi * freq * t)
                for i in range(1, n_sinusoids + 1))
 
 
@@ -18,7 +15,7 @@ def plot_complex_periodic_func():
     plt.figure(dpi=300)
     t = np.linspace(-20,600,500)
     plt.plot(t, complex_periodic_func(t))
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time (ms)')
     plt.ylabel('y-axis')
     plt.title("plot of complex periodic function")
     plt.savefig(plots_dir + 'complex periodic function.png')
@@ -34,7 +31,7 @@ def plot_triangle_func():
     plt.figure(dpi=300)
     t = np.linspace(0,1200,500)
     plt.plot(t, triangle_func(t))
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time (ms)')
     plt.ylabel('y-axis')
     plt.title("plot of triangle function")
     plt.savefig(plots_dir + 'triangle function.png')
@@ -45,7 +42,7 @@ def periodic_func(t, n_sinusoids=4, amp=3., freq=1/300, noise=False):
     """returns a periodic function with n sinosoids"""
     my_sum = sum(amp / i * np.sin(i * np.pi * freq * t) for i in range(1, n_sinusoids + 1))
     if noise:
-        return my_sum + np.random.uniform(0, 0.5, size=len(my_sum))
+        return my_sum + np.random.randn(len(my_sum))*0.5
     else:
         return my_sum
 
@@ -54,16 +51,16 @@ def plot_periodic_func(noise=False):
     plt.figure(dpi=300)
     t = np.linspace(0,1800,500)
     plt.plot(t, periodic_func(t, noise=noise))
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time (ms)')
     plt.ylabel('y-axis')
     plt.title("plot of periodic function")
     plt.savefig(plots_dir + 'periodic function.png')
     plt.show()
 
 
-def square_func(t, noise=False):
+def square_func(t, freq, noise=False):
     """returns a discontinuous function with a square wave"""
-    my_sum = signal.square(2 * np.pi * 5 * t)
+    my_sum = signal.square(2 * np.pi * t*freq*0.5)
     if noise:
         return my_sum + np.random.uniform(0, 0.1, size=len(my_sum))
     else:
@@ -75,7 +72,7 @@ def plot_square_func(noise=False):
     t = np.linspace(0,400,500)
     plt.plot(t, square_func(t,noise=noise))
     plt.ylim(-2, 2)
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time (ms)')
     plt.ylabel('y-axis')
     plt.title("plot of square function")
     plt.savefig(plots_dir + 'square function.png')
@@ -109,6 +106,39 @@ def plot_sine_waves():
     plt.savefig(plots_dir + 'sinus functions.png')
     plt.show()
 
+class lorenz:
+
+    def __init__(self,t, sigma, rho, beta, x0):
+
+        self.t = t
+        self.sigma = sigma
+        self.rho = rho
+        self.beta = beta
+
+        self.x0 = x0
+
+    def f(self, x, t):
+
+        return [self.sigma*(x[1] - x[0]), x[0]*(self.rho-x[2])-x[1], x[0]*x[1]-self.beta*x[2]]
+
+    def integrate(self):
+
+        return odeint(self.f, self.x0,self.t/30).transpose()
+
+def plot_lorenz():
+    t = np.linspace(0,10,10000)
+    ode = lorenz(t,x0=np.array([10,10,10]),rho=28,sigma=10,beta=8/3)
+    solution = ode.integrate() # 3 x 10000
+
+    #3D plot (for fun)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.plot(solution[0],solution[1],solution[2])
+
+    #2D plot
+    plt.figure()
+    plt.plot(t, solution.transpose())
+    plt.show()
 
 script_dir = os.path.dirname(__file__)
 plots_dir = os.path.join(script_dir, 'plots/')
